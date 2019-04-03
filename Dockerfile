0c64369a191b626b4dfb100b490f32c4992d949d
syntacticNaCl <garrettrappaport@protonmail.ch>
2019-04-02 22:05:47 -0400

    Update hash upgrade demo and fix php7.3 errors

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,7 @@ RUN apt-get update && apt-get install -y \
     build-essential \
     mysql-client \
     libpng-dev \
+    libzip-dev \
     libjpeg62-turbo-dev \
     libfreetype6-dev \
     locales \
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,36 +42,29 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
-        $user = User::where('email', $request->email)->get();
-        $verified = $this->verifyOrUpgrade($user, $request);
+        $user = User::where('email', $request->email)->first();
+        $this->resetBcrypt($user, $request);
+        // $verified = $this->verifyOrUpgrade($user, $request);
 
-        if ($verified) {
-            echo "<p>Access granted</p>";
-        } else {
-            echo "<p>Uh uh uh... You didn't say the magic word</p>";
-        }
+        // if ($verified) {
+        //     echo "<p>Access granted</p>";
+        // } else {
+        //     echo "<p>Uh uh uh... You didn't say the magic word</p>";
+        // }
     }
 
-    public function verifyOrUpgrade($user, $request): bool
+    public function verifyOrUpgrade(User $user, Request $request): bool
     {
         // Get stored user password hash
         $row = DB::table('users')->where('email', $request->email)->select('password')->get();
         $storedPasswordHash = $row[0]->password;
 
-        // Hash password from user login form
-        $possibleUserPasswordHash = password_hash($request->password, PASSWORD_BCRYPT, [
-            'cost' => 10,
-        ]);
-
-        // Check to see if password needs to be rehashed
-        $needsRehash = password_needs_rehash($possibleUserPasswordHash, PASSWORD_ARGON2ID, [
+        // Check to see if stored password needs to be rehashed
+        $needsRehash = password_needs_rehash($storedPasswordHash, PASSWORD_ARGON2ID, [
             'memory' => 1024,
             'threads' => 2,
             'time' => 2,
         ]);
-        var_dump($needsRehash);
-
-        return true;
 
         if ($needsRehash) {
             $bcrypt = password_hash($request->password, PASSWORD_BCRYPT, [
@@ -79,15 +72,14 @@ class LoginController extends Controller
             ]);
 
             // Verify current hash
-            $verified = password_verify($request->password, $storedPasswordHash);
+            $verified = password_verify($request->password, $bcrypt);
 
-            if (!$oldPasswordVerify) {
+            // If verification fails then access denied
+            if (!$verified) {
                 return false;
             }
 
-            // Update to new hash
-
-            // Using Laravel defaults
+            // Update to new hash using Laravel defaults
             // Remember these defaults should be dependent on your needs. Go as high as you can comfortably
             $passwordHash = password_hash($request->password, PASSWORD_ARGON2ID, [
                 'memory' => 1024,
@@ -95,13 +87,28 @@ class LoginController extends Controller
                 'time' => 2,
             ]);
 
-            // $user->password = $passwordHash;
-            // $user->save();
+            $user->password = $passwordHash;
+            $user->save();
 
             // Set new password hash to stored so we can verify again
             $storedPasswordHash = $passwordHash;
+
+            echo "brypt upgraded to argon2id";
         }
 
         return password_verify($request->password, $storedPasswordHash);
+    }
+
+    public function resetBcrypt(User $user, Request $request): bool
+    {
+        // Hash password from user login form
+        $bcryptHash = password_hash($request->password, PASSWORD_BCRYPT, [
+            'cost' => 10,
+        ]);
+
+        $user->password = $bcryptHash;
+        $user->save();
+
+        return true;
     }
 }
